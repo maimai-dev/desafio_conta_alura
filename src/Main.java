50ea01b34d2ffd0b45a1752c6f528b7ad9706fa7
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2024-09-09 18:16:04 -0300

    commit 09/09

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String nomeCliente = "Maiara Barbosa Nunes";
+        String tipoConta = "Corrente";
+        double saldoInicial = 2000;
+        String asteriscos = "*********************************";
+
+        System.out.println(asteriscos);
+        System.out.println("Dados iniciais do cliente: ");
+        System.out.println("Nome: " + nomeCliente);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo inicial: " + saldoInicial);
+        System.out.println(asteriscos);
+
+    }
+}
